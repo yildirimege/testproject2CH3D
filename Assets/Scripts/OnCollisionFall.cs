8975f6d582e6d9518db1d7895a544c15f9c969fc
ege-yildirim <ege.yildirimm24@gmail.com>
2020-02-20 01:27:41 +0300

    TestProject2 Color Hole3D
    To Do List:
    
    Fix sketchy player movement.
    
    Add tile map and remove collisions on tilemap under the player to make objects fall inside the hole more realistically
    
    Add score and success panel.
    
    Add Directional Light
    
    Level Design

---
+++ Assets/Scripts/OnCollisionFall.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class OnCollisionFall : MonoBehaviour
+{
+    public GameObject enemy;
+    private int score = 0;
+    public GameObject panel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "white")
+        {
+            collider.GetComponent<Rigidbody>().isKinematic = false;
+         
+
+        }
+
+        else if(collider.gameObject.tag == "enemy")
+        {
+          
+            panel.SetActive(true);
+            collider.GetComponent<Rigidbody>().isKinematic = false;
+
+
+        }
+
+        
+
+
+        
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+
+
+
+    }
+
+    public void OnTriggerExit(Collider bottom)
+    {
+        if(bottom.transform.position.y < 0)
+        {
+
+            bottom.gameObject.SetActive(false);
+            score += 10;
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 moveDirection;
+    private Vector3 startPos;
+
+    private Vector3 direction;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector3 mousePos = Input.mousePosition;
+        if(Input.GetMouseButtonDown(0)) {
+
+           startPos = mousePos;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+          
+            moveDirection = (mousePos - startPos).normalized;
+            var direction = new Vector3(moveDirection.x, 0, moveDirection.y);
+            gameObject.transform.position += direction * speed * Time.deltaTime;
+
+
+
+
+        }
+        
+
+
+    }
+}
